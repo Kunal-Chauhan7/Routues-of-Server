18a1bdeba4bb38a67722a9c3922adb0e8a3a7199
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-06-14 22:56:24 +0530

    added Del

--- index.js
+++ index.js
@@ -20,7 +20,36 @@ http
             }).on('end',()=>{
                 body = JSON.parse(body);
                 console.log(body);
+                let newToDo = activites;
+                newToDo.push(body["name"]);
+                newToDo.push(body.Age);
+                console.log(newToDo);
+                res.writeHead(201);
             });
+        }
+        else if(method === "DELETE"){
+            let body = "";
+            req.on('error',(err)=>{
+                console.error(err);
+            }).on('data',(chunk)=>{
+                body+= chunk;
+            }).on('end',()=>{
+                body = JSON.parse(body);
+                //let todel = body["item"];
+                let todel = body.item;
+                //for (let i = 0; i < activites.length; i++) {
+                //    if(todel === activites[i]){
+                //        activites.splice(i,1);
+                //        console.log(activites);
+                //    }
+                //}
+                activites.find((element,index)=>{
+                    if(element === todel){
+                        activites.splice(index,1);
+                    }
+                })
+            })
+        res.writeHead(204);
         }
     }
     else{

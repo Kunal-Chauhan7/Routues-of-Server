e66d225c282f843c74c5c8684415955b4030c138
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-06-14 15:24:10 +0530

    made routes

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const http = require("http");
 const port = 7770;
+const activites = ["code","games","music"];
 
 http
 .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(`<h1>Hello World! </h1>`);
+    const {method,url} = req;
+    if(url === "/todos"){
+        if(method === "GET"){
+            res.writeHead(200);
+            res.write(activites.toString());
+        }
+        else if(method === "POST"){
+            let body = "";
+            req.on('error',(err)=>{
+                console.log(err);
+            }).on('data',(chunk)=>{
+                body+=chunk;
+                console.log(chunk);
+            }).on('end',()=>{
+                body = JSON.parse(body);
+                console.log(body);
+            });
+        }
+    }
+    else{
+        res.writeHead(404);
+    }
     res.end();
     })
 .listen(port, () => {
